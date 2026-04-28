7ff0672e1dcd4691bc32409b79a642cbd201828a
Vigneshpb <vicky421842@gmail.com>
2026-04-28 16:21:02 +0530

    hi

---
+++ Python.py
@@ -0,0 +1,19 @@
+#print("Hello World")
+
+#name = input("Enter your name:")
+#age = input("Enter your age:")
+#print("My name is",name)
+
+number = int(input())
+
+number2 = int(input())
+
+result = number / number2
+result1 = number * number2
+result2 = number + number2
+result3 = number - number2
+
+print("the number is",result)
+print(result1)
+print(result2)
+print(result3)
